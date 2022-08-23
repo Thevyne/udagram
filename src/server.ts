c86bbf2fd8f623fe258f964e80dc4746d9162ae0
Thevyne <divinegeorge023@gmail.com>
2022-08-23 17:44:33 +0100

    fixed a typo

--- src/server.ts
+++ src/server.ts
@@ -30,7 +30,7 @@ import {Router, Request, Response} from 'express';
 
   /**************************************************************************** */
   app.get('/filteredimage', async (req: Request, res: Response) => {
-    const image_url = req.query.image_url.toSrting();
+    const image_url = req.query.image_url.toString();
     if (!image_url) {
       res.status(400).send('image url is required');
     }
@@ -39,6 +39,7 @@ import {Router, Request, Response} from 'express';
       deleteLocalFiles([filtered_image]);
     });
   });
+
 
   //! END @TODO1
   
